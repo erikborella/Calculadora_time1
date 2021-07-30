dad02cdc825c22e0755838514617b40b0d463ea0
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-07-30 14:48:18 -0300

    Implementação while

--- Calculadora_times/Program.cs
+++ Calculadora_times/Program.cs
@@ -10,71 +10,77 @@ namespace Calculadora_times
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Digite o que deseja fazer:\n" +
-                "1. Somar\n" +
-                "2. Subtrair\n" +
-                "3. Multiplicar\n" +
-                "4. Divisao");
-
-            int operacao = Convert.ToInt32(Console.ReadLine());
-
-            if (operacao != 1 && operacao != 2 && operacao != 3 && operacao != 4 && operacao != 5 && operacao != 6)
+            while (true)
             {
-                Console.WriteLine("Operação inválida, tente novamente");                
+                Console.WriteLine("Digite o que deseja fazer:\n" +
+                    "1. Somar\n" +
+                    "2. Subtrair\n" +
+                    "3. Multiplicar\n" +
+                    "4. Divisao");
+
+                int operacao = Convert.ToInt32(Console.ReadLine());
+
+                if (operacao != 1 && operacao != 2 && operacao != 3 && operacao != 4 && operacao != 5 && operacao != 6)
+                {
+                    Console.WriteLine("Operação inválida, tente novamente");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+
+                Console.WriteLine("Digite o primerio valor: ");
+                int n1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Digite o segundo valor: ");
+                int n2 = Convert.ToInt32(Console.ReadLine());
+
+
+                double resultado = 0;
+
+                switch (operacao)
+                {
+                    case 1:
+                        resultado = n1 + n2;
+                        break;
+
+                    case 2:
+                        resultado = n1 - n2;
+                        break;
+
+                    case 3:
+                        resultado = n1 * n2;
+                        break;
+
+                    case 4:
+                        resultado = n1 / n2;
+                        break;
+                }
+
+                string simboloOperacao = "";
+
+                switch (operacao)
+                {
+                    case 1:
+                        simboloOperacao = "+";
+                        break;
+
+                    case 2:
+                        simboloOperacao = "-";
+                        break;
+
+                    case 3:
+                        simboloOperacao = "*";
+                        break;
+
+                    case 4:
+                        simboloOperacao = "/";
+                        break;
+                }
+
+                Console.WriteLine($"O resultado é: {n1} {simboloOperacao} {n2} = {resultado}");
+                Console.ReadLine();
+
+             }
             }
-
-            Console.WriteLine("Digite o primerio valor: ");
-            int n1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Digite o segundo valor: ");
-            int n2 = Convert.ToInt32(Console.ReadLine());
-            
-
-            double resultado = 0;
-
-            switch (operacao)
-            {
-                case 1:
-                    resultado = n1 + n2;
-                    break;
-
-                case 2:
-                    resultado = n1 - n2;
-                    break;
-
-                case 3:
-                    resultado = n1 * n2;
-                    break;
-
-                case 4:
-                    resultado = n1 / n2;
-                    break;
-            }
-
-            string simboloOperacao = "";
-
-            switch (operacao)
-            {
-                case 1:
-                    simboloOperacao = "+";
-                    break;
-
-                case 2:
-                    simboloOperacao = "-";
-                    break;
-
-                case 3:
-                    simboloOperacao = "*";
-                    break;
-
-                case 4:
-                    simboloOperacao = "/";
-                    break;
-            }
-
-            Console.WriteLine($"O resultado é: {n1} {simboloOperacao} {n2} = {resultado}");
-            Console.ReadLine();
         }
     }
-}

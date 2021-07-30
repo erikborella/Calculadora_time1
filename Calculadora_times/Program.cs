a0ec4cc4a708e08b0cacf76d5ed48c95c2433ad3
Erik Borela <erikborella@gmail.com>
2021-07-30 14:15:38 -0300

    Add project files.

---
+++ Calculadora_times/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o que deseja fazer:\n" +
+                "1. Somar\n" +
+                "2. Subtrair\n" +
+                "3. Multiplicar\n" +
+                "4. Divisao");
+
+            int operacao = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o primerio valor: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            double resultado = 0;
+
+            switch (operacao)
+            {
+                //operacoes
+            }
+
+            Console.WriteLine($"O resultado é: {resultado}");
+        }
+    }
+}
